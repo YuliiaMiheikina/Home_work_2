dd96548651b81707bb04d9df92e53df82e522fde
Yuliia <dgessika18@mail.ru>
2022-10-23 21:17:34 +0300

    nothing

--- HW_3/Task_21/Program.cs
+++ HW_3/Task_21/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек 
 //и находит расстояние между ними в 3D пространстве.
-int[,] array= new int [2,3];
+/*int[,] array= new int [2,3];
 Console.WriteLine("Введите координату x1: ");
 array[0,0] = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите координату y1: ");
@@ -16,7 +16,7 @@ Console.WriteLine("Введите координату z2: ");
 array[1,2] = int.Parse(Console.ReadLine());
 
 double result=Math.Sqrt(Math.Pow((array[0,0]-array[1,0]),2)+Math.Pow((array[0,1]-array[1,1]),2)+Math.Pow((array[0,2]-array[1,2]),2));
-Console.WriteLine(result); 
+Console.WriteLine(result); */
 
 
 
@@ -43,9 +43,7 @@ Console.WriteLine(result);
 
 
 
-
-//НЕ рабочий код(
-/*Console.WriteLine("Введите координаты первой точки: ");
+Console.WriteLine("Введите координаты первой точки: ");
 int x1 = int.Parse(Console.ReadLine());
 int y1 = int.Parse(Console.ReadLine());
 int z1 = int.Parse(Console.ReadLine());
@@ -56,7 +54,7 @@ int y2 = int.Parse(Console.ReadLine());
 int z2 = int.Parse(Console.ReadLine());
 int[] p2 = new int[] {x2, y2, z2};
 
-double result = Math.Sqrt(Math.Pow((p1[0]-p2[0],2))+ Math.Pow((p1[1]-p2[1],2)) + Math.Pow((p1[2]-p2[2],2)));
+double result = Math.Sqrt(Math.Pow((p1[0]-p2[0]),2))+ Math.Pow((p1[1]-p2[1]),2) + Math.Pow((p1[2]-p2[2]),2);
 
-Console.WriteLine(result);*/
+Console.WriteLine(result);
 
