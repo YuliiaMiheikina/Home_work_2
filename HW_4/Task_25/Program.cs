96bc9c8c52a2eebc340e39125c8028fc232da87b
Yuliia <dgessika18@mail.ru>
2022-10-23 23:56:19 +0300

    hw_4

---
+++ HW_4/Task_25/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число A: ");
+double A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+double result = 1;
+for(int i=1;i<=B;i++)
+
+{result=result*A;}
+
+ Console.WriteLine(result);
+
---
+++ HW_4/Task_29/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Massiv();
+void Massiv()
+{
+int [] array = new int[8];
+     
+for( int i=0; i< array.Length;i++)
+{
+    array[i]=new Random().Next(12,19);
+    Console.Write($"{array[i]} ");
+}
+}
+
+
+
+
+
+
