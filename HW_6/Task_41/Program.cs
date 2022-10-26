7b38f5925222a4073592f4e10325cb474defbb1f
Yuliia <dgessika18@mail.ru>
2022-10-26 20:53:55 +0300

    hw_6

---
+++ HW_6/Task_41/Program.cs
@@ -0,0 +1,31 @@
+﻿//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел: ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+}
+
+int FindCountElements(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+PrintArray(numbers);
+Console.WriteLine();
+Console.WriteLine($"Кол-во элементов > 0: " + (FindCountElements(numbers)));
+
+
